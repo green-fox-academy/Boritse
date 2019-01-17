fd021bbf728b13b212e2317b253895c36a833523
Boritse <szakacs.boro@gmail.com>
2019-01-17 22:28:37 +0100

    swaps first and third element of an array

---
+++ week-01/day-4/17_swapElements/cmake-build-debug/CMakeFiles/17_swapElements.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/17_swapElements.dir/main.cpp.obj"
+  "17_swapElements.pdb"
+  "17_swapElements.exe"
+  "17_swapElements.exe.manifest"
+  "lib17_swapElements.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/17_swapElements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
