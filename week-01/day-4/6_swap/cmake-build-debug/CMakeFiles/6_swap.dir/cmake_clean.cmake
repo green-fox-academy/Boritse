72ba6d3a6b3ed9e8af932ef7247a4a3af4006474
Boritse <szakacs.boro@gmail.com>
2019-01-17 21:04:07 +0100

    swaps values of two variables

---
+++ week-01/day-4/6_swap/cmake-build-debug/CMakeFiles/6_swap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/6_swap.dir/main.cpp.obj"
+  "6_swap.pdb"
+  "6_swap.exe"
+  "6_swap.exe.manifest"
+  "lib6_swap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_swap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
