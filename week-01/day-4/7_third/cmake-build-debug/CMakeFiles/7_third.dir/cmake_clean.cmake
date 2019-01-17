eda894cc6ba27dc44f1fac22aa000accea830d4f
Boritse <szakacs.boro@gmail.com>
2019-01-17 21:07:41 +0100

    prints third element of array

---
+++ week-01/day-4/7_third/cmake-build-debug/CMakeFiles/7_third.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/7_third.dir/main.cpp.obj"
+  "7_third.pdb"
+  "7_third.exe"
+  "7_third.exe.manifest"
+  "lib7_third.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7_third.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
