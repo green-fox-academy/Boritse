0772e5fbc3913b81ef28676cff0df5ee9613d831
Boritse <szakacs.boro@gmail.com>
2019-01-17 21:43:33 +0100

    adds up two elements of an array

---
+++ week-01/day-4/9_sumElements/cmake-build-debug/CMakeFiles/9_sumElements.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/9_sumElements.dir/main.cpp.obj"
+  "9_sumElements.pdb"
+  "9_sumElements.exe"
+  "9_sumElements.exe.manifest"
+  "lib9_sumElements.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/9_sumElements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
