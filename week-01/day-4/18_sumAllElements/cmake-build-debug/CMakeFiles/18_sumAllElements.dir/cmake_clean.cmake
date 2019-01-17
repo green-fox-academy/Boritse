d1dbd8e84357fdacb317568080c9974da998703f
Boritse <szakacs.boro@gmail.com>
2019-01-17 22:33:10 +0100

    adds up all elements of an array

---
+++ week-01/day-4/18_sumAllElements/cmake-build-debug/CMakeFiles/18_sumAllElements.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/18_sumAllElements.dir/main.cpp.obj"
+  "18_sumAllElements.pdb"
+  "18_sumAllElements.exe"
+  "18_sumAllElements.exe.manifest"
+  "lib18_sumAllElements.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/18_sumAllElements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
