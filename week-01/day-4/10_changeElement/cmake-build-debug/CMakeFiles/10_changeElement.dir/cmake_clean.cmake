593daab7a3fbf54796d42f1e543c3fc481f06661
Boritse <szakacs.boro@gmail.com>
2019-01-17 21:46:45 +0100

    changes an element

---
+++ week-01/day-4/10_changeElement/cmake-build-debug/CMakeFiles/10_changeElement.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/10_changeElement.dir/main.cpp.obj"
+  "10_changeElement.pdb"
+  "10_changeElement.exe"
+  "10_changeElement.exe.manifest"
+  "lib10_changeElement.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10_changeElement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
