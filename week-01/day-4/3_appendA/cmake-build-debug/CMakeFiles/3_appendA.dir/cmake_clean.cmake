ce6084de97103ade1ef2f7a32840fe9a60ba77de
Boritse <szakacs.boro@gmail.com>
2019-01-17 20:27:37 +0100

    adds an 'a' to the given string variable

---
+++ week-01/day-4/3_appendA/cmake-build-debug/CMakeFiles/3_appendA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3_appendA.dir/main.cpp.obj"
+  "3_appendA.pdb"
+  "3_appendA.exe"
+  "3_appendA.exe.manifest"
+  "lib3_appendA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_appendA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
