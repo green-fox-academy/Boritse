630425c57f4e651de29f0a9719215d70e2a0238c
Boritse <szakacs.boro@gmail.com>
2019-01-17 20:18:54 +0100

    it doubles the given value

---
+++ week-01/day-4/1_doubling/cmake-build-debug/CMakeFiles/1_doubling.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1_doubling.dir/main.cpp.obj"
+  "1_doubling.pdb"
+  "1_doubling.exe"
+  "1_doubling.exe.manifest"
+  "lib1_doubling.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_doubling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
