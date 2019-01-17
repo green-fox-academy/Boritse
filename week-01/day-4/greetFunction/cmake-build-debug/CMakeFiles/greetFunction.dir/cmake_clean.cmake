0c08b9dc2b6fb7fa4ed3b227b6fdcf3c60c303c6
Boritse <szakacs.boro@gmail.com>
2019-01-17 20:23:32 +0100

    greets the input string variable

---
+++ week-01/day-4/greetFunction/cmake-build-debug/CMakeFiles/greetFunction.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greetFunction.dir/main.cpp.obj"
+  "greetFunction.pdb"
+  "greetFunction.exe"
+  "greetFunction.exe.manifest"
+  "libgreetFunction.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greetFunction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
