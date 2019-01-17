8aa0e105f13ba6698079bc0efd1fb79b8207f142
Boritse <szakacs.boro@gmail.com>
2019-01-17 21:50:11 +0100

    increments an element

---
+++ week-01/day-4/11_incrementElement/cmake-build-debug/CMakeFiles/11_incrementElement.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/11_incrementElement.dir/main.cpp.obj"
+  "11_incrementElement.pdb"
+  "11_incrementElement.exe"
+  "11_incrementElement.exe.manifest"
+  "lib11_incrementElement.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11_incrementElement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
