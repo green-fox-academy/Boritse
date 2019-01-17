d48e700bd1ca18f517d220800aec73cc6e935883
Boritse <szakacs.boro@gmail.com>
2019-01-17 21:21:52 +0100

    compares length of two arrays

---
+++ week-01/day-4/8_compareLength/cmake-build-debug/CMakeFiles/8_compareLength.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/8_compareLength.dir/main.cpp.obj"
+  "8_compareLength.pdb"
+  "8_compareLength.exe"
+  "8_compareLength.exe.manifest"
+  "lib8_compareLength.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_compareLength.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-01/day-4/8_compareLength/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+void sizeCompare(int x[], int y[]){
+    if ((sizeof(x) / sizeof(x[0])) > (sizeof(y)/sizeof(y[0]))){
+        std::cout << x << " has more elements than " << y << std::endl;
+    }else{
+        std::cout << y << " has more elements than " << x << std::endl;
+    }
+}
+
+int main(int argc, char* args[]) {
+
+    int p1[]={1,2,3};
+    int p2[]={4,5};
+
+    sizeCompare(p1, p2);
+    // - Create an array variable named `p1`
+    //   with the following content: `[1, 2, 3]`
+    // - Create an array variable named `p2`
+    //   with the following content: `[4, 5]`
+    // - Print if `p2` has more elements than `p1`
+
+    return 0;
+}
