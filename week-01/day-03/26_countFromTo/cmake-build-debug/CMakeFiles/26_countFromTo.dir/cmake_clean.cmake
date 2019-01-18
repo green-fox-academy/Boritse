5f844741af986f4aeb472e0bcb6ea50684adf070
Boritse <szakacs.boro@gmail.com>
2019-01-18 13:58:56 +0100

    takes two number and does stuff

---
+++ week-01/day-03/26_countFromTo/cmake-build-debug/CMakeFiles/26_countFromTo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/26_countFromTo.dir/main.cpp.obj"
+  "26_countFromTo.pdb"
+  "26_countFromTo.exe"
+  "26_countFromTo.exe.manifest"
+  "lib26_countFromTo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/26_countFromTo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- week-01/day-4/5_factoral/main.cpp
+++ week-01/day-4/5_factoral/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <string>
 
-int factorio(int x, int y){
+int factorio(int& x, int& y){
     int w=1;
     for(int z=1; z<=x; z++){
         w*=z;
     }
     y=w;
-    std::cout << x << " " << y << std::endl;
 }
 
 int main(int argc, char* args[]) {
 
-    std::cout << factorio(4,7) << std::endl;
+    int x=4;
+    int y=7;
+    factorio(x,y);
+    std::cout << x << " " << y << std::endl;
     // - Create a function called `factorio`
     //   it should calculate its input's factorial.
     //   it should not return it, but take an additional integer parameter and overwrite its value.
