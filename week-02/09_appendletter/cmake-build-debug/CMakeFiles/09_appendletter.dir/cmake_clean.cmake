2669f678a2d10feffb27d26a4ae9534201f9210e
Boritse <szakacs.boro@gmail.com>
2019-01-21 15:36:18 +0100

    adds an 'a' to all elements of vector

---
+++ week-02/09_appendletter/cmake-build-debug/CMakeFiles/09_appendletter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/09_appendletter.dir/main.cpp.obj"
+  "09_appendletter.pdb"
+  "09_appendletter.exe"
+  "09_appendletter.exe.manifest"
+  "lib09_appendletter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/09_appendletter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
