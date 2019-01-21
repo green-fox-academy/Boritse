7a97fe4f5b5b706fa9f8b0b4e35cb422ab4c1c62
Boritse <szakacs.boro@gmail.com>
2019-01-21 15:31:15 +0100

    adds string in the middle of a vector

---
+++ week-02/07_solarsystem/cmake-build-debug/CMakeFiles/07_solarsystem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/07_solarsystem.dir/main.cpp.obj"
+  "07_solarsystem.pdb"
+  "07_solarsystem.exe"
+  "07_solarsystem.exe.manifest"
+  "lib07_solarsystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/07_solarsystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
