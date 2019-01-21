df43b102a830a83de572065f3bbb14231bf6b795
Boritse <szakacs.boro@gmail.com>
2019-01-21 13:18:41 +0100

    replaces two substrings in a string

---
+++ week-02/01_simplereplace/cmake-build-debug/CMakeFiles/01_simplereplace.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/01_simplereplace.dir/main.cpp.obj"
+  "01_simplereplace.pdb"
+  "01_simplereplace.exe"
+  "01_simplereplace.exe.manifest"
+  "lib01_simplereplace.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_simplereplace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
