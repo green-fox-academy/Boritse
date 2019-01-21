f54e50e180581896f27749fde1894201231e884e
Boritse <szakacs.boro@gmail.com>
2019-01-21 13:31:49 +0100

    fixes wrong url

---
+++ week-02/02_urlfixer/cmake-build-debug/CMakeFiles/02_urlfixer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/02_urlfixer.dir/main.cpp.obj"
+  "02_urlfixer.pdb"
+  "02_urlfixer.exe"
+  "02_urlfixer.exe.manifest"
+  "lib02_urlfixer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02_urlfixer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
