b14fb437a4fd469181d682fe28c15c52ef4128aa
Boritse <szakacs.boro@gmail.com>
2019-01-21 17:18:24 +0100

    checks if all vector elements are in other vector too

---
+++ week-02/isinlist/cmake-build-debug/CMakeFiles/isinlist.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/isinlist.dir/main.cpp.obj"
+  "isinlist.pdb"
+  "isinlist.exe"
+  "isinlist.exe.manifest"
+  "libisinlist.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/isinlist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
