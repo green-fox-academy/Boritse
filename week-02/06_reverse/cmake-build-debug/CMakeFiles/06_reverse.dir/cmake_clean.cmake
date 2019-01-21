755c6c6f9cc212ad59a554dca30e5c8a90215a79
Boritse <szakacs.boro@gmail.com>
2019-01-21 14:11:25 +0100

    reverses string

---
+++ week-02/06_reverse/cmake-build-debug/CMakeFiles/06_reverse.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/06_reverse.dir/main.cpp.obj"
+  "06_reverse.pdb"
+  "06_reverse.exe"
+  "06_reverse.exe.manifest"
+  "lib06_reverse.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/06_reverse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
