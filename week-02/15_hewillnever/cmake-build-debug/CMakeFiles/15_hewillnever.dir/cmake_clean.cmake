dc66e4c84f0b48ba9910065c20e0062eaebd03db
Boritse <szakacs.boro@gmail.com>
2019-01-21 16:09:03 +0100

    makes you feel the music

---
+++ week-02/15_hewillnever/cmake-build-debug/CMakeFiles/15_hewillnever.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/15_hewillnever.dir/main.cpp.obj"
+  "15_hewillnever.pdb"
+  "15_hewillnever.exe"
+  "15_hewillnever.exe.manifest"
+  "lib15_hewillnever.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15_hewillnever.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
