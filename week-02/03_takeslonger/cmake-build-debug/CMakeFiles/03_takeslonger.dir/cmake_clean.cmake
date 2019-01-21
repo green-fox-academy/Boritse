9bc2717cf12131ede15e4c137052092714854b3c
Boritse <szakacs.boro@gmail.com>
2019-01-21 13:40:42 +0100

    Hofstadter's Law

---
+++ week-02/03_takeslonger/cmake-build-debug/CMakeFiles/03_takeslonger.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/03_takeslonger.dir/main.cpp.obj"
+  "03_takeslonger.pdb"
+  "03_takeslonger.exe"
+  "03_takeslonger.exe.manifest"
+  "lib03_takeslonger.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/03_takeslonger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
