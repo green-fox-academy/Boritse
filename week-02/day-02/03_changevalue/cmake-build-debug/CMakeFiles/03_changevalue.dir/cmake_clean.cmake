d4fa093c320301fe45331aa927aa56ae1958936a
Boritse <szakacs.boro@gmail.com>
2019-01-22 13:56:36 +0100

    changing value of variable using a pointer

---
+++ week-02/day-02/03_changevalue/cmake-build-debug/CMakeFiles/03_changevalue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/03_changevalue.dir/main.cpp.obj"
+  "03_changevalue.pdb"
+  "03_changevalue.exe"
+  "03_changevalue.exe.manifest"
+  "lib03_changevalue.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/03_changevalue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
