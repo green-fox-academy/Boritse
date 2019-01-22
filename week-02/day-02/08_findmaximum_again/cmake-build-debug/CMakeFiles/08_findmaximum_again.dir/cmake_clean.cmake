53df1999e5f76ba967e0a35592a5b29d84dca7cf
Boritse <szakacs.boro@gmail.com>
2019-01-22 16:03:20 +0100

    finds biggest element even within negative numbers

---
+++ week-02/day-02/08_findmaximum_again/cmake-build-debug/CMakeFiles/08_findmaximum_again.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/08_findmaximum_again.dir/main.cpp.obj"
+  "08_findmaximum_again.pdb"
+  "08_findmaximum_again.exe"
+  "08_findmaximum_again.exe.manifest"
+  "lib08_findmaximum_again.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/08_findmaximum_again.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
