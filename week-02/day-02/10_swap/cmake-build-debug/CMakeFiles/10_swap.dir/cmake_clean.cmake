34c3665d0a27a4f219556468734f11cbae1d0d61
Boritse <szakacs.boro@gmail.com>
2019-01-22 16:36:26 +0100

    swaps two numbers

---
+++ week-02/day-02/10_swap/cmake-build-debug/CMakeFiles/10_swap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/10_swap.dir/main.cpp.obj"
+  "10_swap.pdb"
+  "10_swap.exe"
+  "10_swap.exe.manifest"
+  "lib10_swap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/10_swap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-02/day-02/10_swap/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void swapper(int &firstNumber, int &secondNumber){
+    int helper = firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = helper;
+}
+
+int main ()
+{
+    // Create a function which swaps the values of 'a' and 'b'
+    // This time use a void funtion and pointers
+
+    int a = 10;
+    int b = 316;
+
+    std::cout << "The value of a is: " << a << ", and the value of b is: " << b << std::endl;
+
+    int* p_a = &a;
+    int* p_b = &b;
+    
+    swapper(*p_a, *p_b);
+
+    std::cout << "And now the value of a is: " << a << ", and the value of b is: " << b;
+
+    return 0;
+}
