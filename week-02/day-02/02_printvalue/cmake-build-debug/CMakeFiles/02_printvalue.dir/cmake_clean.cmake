dab341ab26e316b1c26a39a8fd73836ec58503d2
Boritse <szakacs.boro@gmail.com>
2019-01-22 13:52:08 +0100

    printin out a value using a pointer pointing to it

---
+++ week-02/day-02/02_printvalue/cmake-build-debug/CMakeFiles/02_printvalue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/02_printvalue.dir/main.cpp.obj"
+  "02_printvalue.pdb"
+  "02_printvalue.exe"
+  "02_printvalue.exe.manifest"
+  "lib02_printvalue.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02_printvalue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
