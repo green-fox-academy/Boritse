565062958f3cd6cd17d6569990c4242d31e0bb2d
Boritse <szakacs.boro@gmail.com>
2019-01-23 20:34:12 +0100

    half working again

---
+++ week-02/day-03/06_copyFile_other/cmake-build-debug/CMakeFiles/06_copyFile_other.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/06_copyFile_other.dir/main.cpp.obj"
+  "06_copyFile_other.pdb"
+  "06_copyFile_other.exe"
+  "06_copyFile_other.exe.manifest"
+  "lib06_copyFile_other.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/06_copyFile_other.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-02/day-03/06_copyFile_other/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <exception>
+#include <vector>
+#include <sstream>
+
+
+void copier(std::string originalFile, std::string copyFile);
+
+int main(){
+
+    std::string file1="original.txt";
+    std::string file2="copy.txt";
+    copier(file1, file2);
+
+    return 0;
+}
+
+void copier(std::string originalFile, std::string copyFile){
+
+    std::string lines;
+    std::ifstream readOutFile(originalFile);
+    std::ofstream writeInFile(copyFile);
+
+    while(std::getline(readOutFile, lines)){
+        writeInFile << lines;
+        std::cout << lines << std::endl;
+    }
+    writeInFile.close();
+    readOutFile.close();
+
+}
