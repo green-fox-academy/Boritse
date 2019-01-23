3be41ea0bc5c645c52e7c8cc87a53a38ddac2f09
Boritse <szakacs.boro@gmail.com>
2019-01-23 15:21:48 +0100

    i can write a line in a file

---
+++ week-02/day-03/04_writeSingleLine/cmake-build-debug/CMakeFiles/04_writeSingleLine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/04_writeSingleLine.dir/main.cpp.obj"
+  "04_writeSingleLine.pdb"
+  "04_writeSingleLine.exe"
+  "04_writeSingleLine.exe.manifest"
+  "lib04_writeSingleLine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/04_writeSingleLine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
