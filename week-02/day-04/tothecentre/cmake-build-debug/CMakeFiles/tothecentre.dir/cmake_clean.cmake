6fbc30dc68e710acac75459de7bab4a975d74b36
Boritse <szakacs.boro@gmail.com>
2019-01-24 16:47:16 +0100

    draws three lines to the centre of the window

---
+++ week-02/day-04/tothecentre/cmake-build-debug/CMakeFiles/tothecentre.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tothecentre.dir/main.cpp.obj"
+  "tothecentre.pdb"
+  "tothecentre.exe"
+  "tothecentre.exe.manifest"
+  "libtothecentre.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tothecentre.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
