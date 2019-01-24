7ce42c52bc03bb05737f8771594f6f7a6c59641f
Boritse <szakacs.boro@gmail.com>
2019-01-24 16:08:53 +0100

    draws the two diagonal line

---
+++ week-02/day-04/diagonals/cmake-build-debug/CMakeFiles/colouredbox.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/colouredbox.dir/main.cpp.obj"
+  "colouredbox.pdb"
+  "colouredbox.exe"
+  "colouredbox.exe.manifest"
+  "libcolouredbox.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/colouredbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
