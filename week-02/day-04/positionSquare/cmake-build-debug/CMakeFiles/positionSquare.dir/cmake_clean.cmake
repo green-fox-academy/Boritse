71a41d8d04e8e714f8c5db20e352d9d2d4ceabe7
Boritse <szakacs.boro@gmail.com>
2019-01-24 20:52:34 +0100

    draws squares on the diagonal

---
+++ week-02/day-04/positionSquare/cmake-build-debug/CMakeFiles/positionSquare.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/positionSquare.dir/main.cpp.obj"
+  "positionSquare.pdb"
+  "positionSquare.exe"
+  "positionSquare.exe.manifest"
+  "libpositionSquare.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/positionSquare.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-02/day-04/positionSquare/cmake-build-debug/CMakeFiles/rectangles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rectangles.dir/main.cpp.obj"
+  "rectangles.pdb"
+  "rectangles.exe"
+  "rectangles.exe.manifest"
+  "librectangles.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rectangles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
