80465000512d969cbaf71e903fa45085221236b0
Boritse <szakacs.boro@gmail.com>
2019-01-24 17:12:09 +0100

    draws a green square in the middle of the window

---
+++ week-02/day-04/centredSquare/cmake-build-debug/CMakeFiles/centredSquare.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/centredSquare.dir/main.cpp.obj"
+  "centredSquare.pdb"
+  "centredSquare.exe"
+  "centredSquare.exe.manifest"
+  "libcentredSquare.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/centredSquare.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-02/day-04/centredSquare/cmake-build-debug/CMakeFiles/horizontalLines.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/horizontalLines.dir/main.cpp.obj"
+  "horizontalLines.pdb"
+  "horizontalLines.exe"
+  "horizontalLines.exe.manifest"
+  "libhorizontalLines.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/horizontalLines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
