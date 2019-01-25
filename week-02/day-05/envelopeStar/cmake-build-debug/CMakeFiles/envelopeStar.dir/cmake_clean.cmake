4a5faae78b6d46863f474280b69ee7f78f015a10
Boritse <szakacs.boro@gmail.com>
2019-01-25 11:38:58 +0100

    draws an envelope star, whatever that means

---
+++ week-02/day-05/envelopeStar/cmake-build-debug/CMakeFiles/envelopeStar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/envelopeStar.dir/main.cpp.obj"
+  "envelopeStar.pdb"
+  "envelopeStar.exe"
+  "envelopeStar.exe.manifest"
+  "libenvelopeStar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/envelopeStar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-02/day-05/envelopeStar/cmake-build-debug/CMakeFiles/linePlay.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linePlay.dir/main.cpp.obj"
+  "linePlay.pdb"
+  "linePlay.exe"
+  "linePlay.exe.manifest"
+  "liblinePlay.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linePlay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
