09ce0e605de9dd4cf8690ab5b21b3062c1bd2a62
Boritse <szakacs.boro@gmail.com>
2019-01-25 13:20:17 +0100

    still to be written

---
+++ week-02/day-05/triangles/cmake-build-debug/CMakeFiles/triangles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/triangles.dir/main.cpp.obj"
+  "triangles.pdb"
+  "triangles.exe"
+  "triangles.exe.manifest"
+  "libtriangles.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/triangles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
