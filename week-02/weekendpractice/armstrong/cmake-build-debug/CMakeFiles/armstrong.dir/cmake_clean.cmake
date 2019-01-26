8a54b09ddf231429be395ed50883b20154796728
Boritse <szakacs.boro@gmail.com>
2019-01-27 00:54:07 +0100

    for some unknown reason it doesnt work properly

---
+++ week-02/weekendpractice/armstrong/cmake-build-debug/CMakeFiles/armstrong.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/armstrong.dir/main.cpp.obj"
+  "armstrong.pdb"
+  "armstrong.exe"
+  "armstrong.exe.manifest"
+  "libarmstrong.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/armstrong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-02/weekendpractice/armstrong/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int numberToCheck;
+    std::cout << "Let me check if the number on your mind is an Armstrong number! Which number is on your mind?" << std::endl;
+    std::cin >> numberToCheck;
+
+    std::vector<int> digits;
+    int digit=0;
+
+    while(numberToCheck>0){
+        digit = numberToCheck%10;
+        numberToCheck/=10;
+        digits.push_back(digit);
+    }
+
+    int armstrong=0;
+
+    for(int i=0; i<digits.size(); i++){
+        int currentDigit=1;
+        for(int j=0; j<digits.size(); j++){
+            currentDigit*=digits[i];
+        }
+        armstrong+=currentDigit;
+    }
+
+    if(numberToCheck==armstrong){
+        std::cout << "You are a lucky man, your number is an Armstrong number!";
+    }else{
+        std::cout << "Sorry to say but your number is not an Armstrong number.";
+    }
+    return 0;
+}
