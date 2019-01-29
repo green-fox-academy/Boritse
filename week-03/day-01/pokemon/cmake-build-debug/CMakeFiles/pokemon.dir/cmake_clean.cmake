bc1c99c718a5a3830be50b39d650fbd34448e3de
Boritse <szakacs.boro@gmail.com>
2019-01-29 20:46:58 +0100

    choosesright pokemon to call

---
+++ week-03/day-01/pokemon/cmake-build-debug/CMakeFiles/pokemon.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pokemon.dir/main.cpp.obj"
+  "CMakeFiles/pokemon.dir/pokemon.cpp.obj"
+  "pokemon.pdb"
+  "pokemon.exe"
+  "pokemon.exe.manifest"
+  "libpokemon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pokemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-03/day-01/pokemon/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+#include "pokemon.h"
+
+
+std::vector<Pokemon> initializePokemons()
+{
+    std::vector<Pokemon> pokemons;
+
+    pokemons.push_back(Pokemon("Balbasaur", "leaf", "water"));
+    pokemons.push_back(Pokemon("Pikatchu", "electric", "water"));
+    pokemons.push_back(Pokemon("Charizard", "fire", "leaf"));
+    pokemons.push_back(Pokemon("Balbasaur", "water", "fire"));
+    pokemons.push_back(Pokemon("Kingler", "water", "fire"));
+
+    return pokemons;
+}
+
+int main(int argc, char* args[])
+{
+    std::vector<Pokemon> pokemonOfAsh = initializePokemons();
+
+    // Every pokemon has a name and a type.
+    // Certain types are effective against others, e.g. water is effective against fire.
+
+    // Ash has a few pokemon.
+    // A wild pokemon appeared!
+
+    Pokemon wildPokemon("Oddish", "leaf", "water");
+
+    int rightPokemon =0;
+    for(int i=0; i<pokemonOfAsh.size(); i++){
+       if(pokemonOfAsh[i].isEffectiveAgainst(wildPokemon)==1){
+       rightPokemon=i;}
+    }
+
+
+    // Which pokemon should Ash use?
+
+    std::cout << "I choose You, " << pokemonOfAsh[rightPokemon]._name << std::endl;
+    return 0;
+}
