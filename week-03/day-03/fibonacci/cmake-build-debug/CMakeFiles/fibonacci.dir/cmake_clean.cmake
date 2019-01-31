9f124b95ddb20b60815f72cf9ef834c1deb097ca
Boritse <szakacs.boro@gmail.com>
2019-01-31 17:08:07 +0100

    gives you any element that you want of the fibonacci numbers

---
+++ week-03/day-03/fibonacci/cmake-build-debug/CMakeFiles/fibonacci.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fibonacci.dir/main.cpp.obj"
+  "fibonacci.pdb"
+  "fibonacci.exe"
+  "fibonacci.exe.manifest"
+  "libfibonacci.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fibonacci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-03/day-03/fibonacci/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int fibonacci(int n){
+    if(n==0){
+        return 0;
+    }else if(n==1){
+        return 1;
+    }else{
+        n=fibonacci(n-1)+fibonacci(n-2);
+    }
+    return n;
+}
+
+int main() {
+    std::cout << fibonacci(9);
+    return 0;
+}
